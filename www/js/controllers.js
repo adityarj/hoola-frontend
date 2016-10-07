16bc3db8d0ea4bec32a66b3070af15103e00bf60
Aditya Raju <aditya_raju@mymail.sutd.edu.sg>
2016-10-07 20:38:11 +0800

    css + tidying up

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -197,7 +197,7 @@ angular.module('starter.controllers', [])
         if (Message == '@hoola') {
             //Trigger api.ai 
             $scope.Message = Message+' ';
-        } else if (Message == '@calculator') {
+        } else if (Message == '@money') {
             $scope.showCalculator = true;
         }
     };
@@ -211,25 +211,33 @@ angular.module('starter.controllers', [])
 
     $scope.Reservation = {
         people: 2,
-        city: 'Tokyo, Japan',
+        city: 'Tokyo',
         checkIn: '',
         checkOut: ''
     };
 
     $scope.listings = [];
 
+    
+
     $scope.fetchHotels = function() {
-        $http.get('https://api.airbnb.com/v2/search_results?client_id=3092nxybyb0otqw18e8nh5nty&locale=en-US&currency=USD&_format=for_search_results_with_minimal_pricing&_limit=10&_offset=0&fetch_facets=true&guests=1&ib=false&ib_add_photo_flow=true&location=Lake%20Tahoe%2C%20CA%2C%20US&min_bathrooms=0&min_bedrooms=0&min_beds=1&min_num_pic_urls=10&price_max=210&price_min=40&sort=1&user_lat=37.3398634&user_lng=-122.0455164')
+        $http.get('https://api.airbnb.com/v2/search_results?client_id=3092nxybyb0otqw18e8nh5nty&locale=en-US&currency=SGD&_format=for_search_results_with_minimal_pricing&_limit=10&_offset=0&fetch_facets=true&guests=1&ib=true&ib_add_photo_flow=true&'
+            +'location='+$scope.Reservation.city+'&min_bathrooms=0&min_bedrooms=0&'+
+            +'min_beds='+$scope.Reservation.people+'&min_num_pic_urls=10&price_max=210&price_min=40&sort=1&user_lat=37.3398634&user_lng=-122.0455164')
             .success(function (listings) {
                 $scope.disabledStatus = true;
-                //$scope.listings = listings;
-                console.log(listings);
+                $scope.listings = listings.search_results;
         });
     };
 
-    $scope.bookHotel = function() {
-        //I will finalize booking here and send to the server, then update it back on the function
+    $scope.bookHotel = function(address) {
+        var listing = {
+            checkIn: $scope.Reservation.checkIn,
+            checkOut: $scope.Reservation.checkOut,
+            addresss: address
+        };
     };
+
 })
 
 .controller('FlightController',function($scope,$http) {
